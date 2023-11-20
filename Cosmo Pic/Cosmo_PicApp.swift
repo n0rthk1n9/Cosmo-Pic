7e4676786f04664f63d576b057a2ea3bba351a9e
Jan Armbrust <jan.armbrust@googlemail.com>
2023-11-20 15:48:49 +0100

    Initial Commit

---
+++ Cosmo Pic/Cosmo_PicApp.swift
@@ -0,0 +1,17 @@
+//
+//  Cosmo_PicApp.swift
+//  Cosmo Pic
+//
+//  Created by Jan Armbrust on 20.11.23.
+//
+
+import SwiftUI
+
+@main
+struct Cosmo_PicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Cosmo PicUITests/Cosmo_PicUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Cosmo_PicUITestsLaunchTests.swift
+//  Cosmo PicUITests
+//
+//  Created by Jan Armbrust on 20.11.23.
+//
+
+import XCTest
+
+final class Cosmo_PicUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
