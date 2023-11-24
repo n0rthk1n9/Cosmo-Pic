7c77fe6a905c74725cb5e17519194c8441f1280c
Jan Armbrust <jan.armbrust@googlemail.com>
2023-11-24 19:58:47 +0100

    add swiftlint and fix warnings

--- CosmoPic/CosmoPicApp.swift
+++ CosmoPic/CosmoPicApp.swift
@@ -9,9 +9,9 @@ import SwiftUI
 
 @main
 struct CosmoPicApp: App {
-    var body: some Scene {
-        WindowGroup {
-            ContentView()
-        }
+  var body: some Scene {
+    WindowGroup {
+      ContentView()
     }
+  }
 }
