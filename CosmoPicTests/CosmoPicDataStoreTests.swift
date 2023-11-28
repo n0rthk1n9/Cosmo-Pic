28bf949a6aeff824ed7a43dfab40df69f8951ff7
Jan Armbrust <jan.armbrust@googlemail.com>
2023-11-28 20:43:09 +0100

    add more tests

--- CosmoPicTests/CosmoPicDataStoreTests.swift
+++ CosmoPicTests/CosmoPicDataStoreTests.swift
@@ -13,11 +13,30 @@ final class CosmoPicDataStoreTests: XCTestCase {
 
   override func setUp() {
     dataStore = DataStore(
-      photoAPIService: PhotoAPIServiceMock()
+      photoAPIService: PhotoAPIServiceMock(),
+      historyAPIService: HistoryAPIServiceMock()
     )
   }
 
-  func test_getPhoto() async {
+  func test_getPhoto_forToday() async {
+    let dateFormatter = DateFormatter()
+    dateFormatter.dateFormat = "yyyy-MM-dd"
+    let currentDate = Date()
+    let todayString = dateFormatter.string(from: currentDate)
+    XCTAssertFalse(dataStore.isLoading)
+    XCTAssertNil(dataStore.photo)
+    XCTAssertFalse(dataStore.errorIsPresented)
+    XCTAssertNil(dataStore.error)
+
+    await dataStore.getPhoto(for: todayString)
+
+    XCTAssertFalse(dataStore.isLoading)
+    XCTAssertEqual(dataStore.photo, .allProperties)
+    XCTAssertFalse(dataStore.errorIsPresented)
+    XCTAssertNil(dataStore.error)
+  }
+
+  func test_getPhoto_forOtherDate() async {
     XCTAssertFalse(dataStore.isLoading)
     XCTAssertNil(dataStore.photo)
     XCTAssertFalse(dataStore.errorIsPresented)
@@ -29,5 +48,61 @@ final class CosmoPicDataStoreTests: XCTestCase {
     XCTAssertEqual(dataStore.photo, .allProperties)
     XCTAssertFalse(dataStore.errorIsPresented)
     XCTAssertNil(dataStore.error)
+  }
+
+  func test_getHistory() async {
+    XCTAssertFalse(dataStore.isLoading)
+    XCTAssertEqual(dataStore.history, [])
+    XCTAssertFalse(dataStore.errorIsPresented)
+    XCTAssertNil(dataStore.error)
+
+    await dataStore.getHistory()
+
+    XCTAssertFalse(dataStore.isLoading)
+    XCTAssertEqual(
+      dataStore.history,
+      [
+        .allProperties,
+        .allProperties
+      ]
+    )
+    XCTAssertFalse(dataStore.errorIsPresented)
+    XCTAssertNil(dataStore.error)
+  }
+
+  func test_getHistory_WhenFileDoesNotExist() async {
+    let fileManager = FileManager.default
+    let dateFormatter = DateFormatter()
+    dateFormatter.dateFormat = "yyyy-MM-dd"
+    let currentDate = Date()
+    let todayString = dateFormatter.string(from: currentDate)
+    let historyFilePath = FileManager.documentsDirectoryURL.appendingPathComponent("\(todayString)-history.json")
+    try? fileManager.removeItem(at: historyFilePath)
+
+    await dataStore.getHistory()
+
+    XCTAssertFalse(dataStore.isLoading)
+    XCTAssertEqual(dataStore.history.count, 2)
+    XCTAssertFalse(dataStore.errorIsPresented)
+    XCTAssertNil(dataStore.error)
+  }
+
+  func test_errorIsHandeled() async throws {
+    dataStore = DataStore(
+      photoAPIService: PhotoAPIServiceErrorMock()
+    )
+
+    XCTAssertFalse(dataStore.isLoading)
+    XCTAssertNil(dataStore.photo)
+    XCTAssertFalse(dataStore.errorIsPresented)
+    XCTAssertNil(dataStore.error)
+
+    await dataStore.getPhoto(for: "2023-09-04")
+
+    XCTAssertFalse(dataStore.isLoading)
+    XCTAssertNil(dataStore.photo)
+    XCTAssertTrue(dataStore.errorIsPresented)
+    let error = try XCTUnwrap(dataStore.error)
+    XCTAssertEqual(error.localizedDescription, PhotoAPIServiceErrorMock.SomeError.anError.localizedDescription)
   }
 }
