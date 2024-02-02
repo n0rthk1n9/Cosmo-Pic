5345c82d70bea69c04c87e214c3531f04d6999c7
Jan Armbrust <jan.armbrust@googlemail.com>
2024-02-02 15:56:40 +0100

    use photo api service to fetch photo in new view model

--- CosmoPic/ViewModels/APODViewModel.swift
+++ CosmoPic/ViewModels/APODViewModel.swift
@@ -6,3 +6,52 @@
 //
 
 import Foundation
+import SwiftUI
+
+class APODViewModel: ObservableObject {
+  @Published var photo: Photo?
+  @Published var isLoading = false
+  @Published var error: Error?
+
+  private let photoAPIService: PhotoAPIServiceProtocol
+
+  init(photoAPIService: PhotoAPIServiceProtocol = PhotoAPIService()) {
+    self.photoAPIService = photoAPIService
+  }
+
+  @MainActor
+  func fetchPhotoForToday() async {
+    isLoading = true
+    error = nil
+
+    let dateFormatter = DateFormatter()
+    dateFormatter.dateFormat = "yyyy-MM-dd"
+    let currentDate = dateFormatter.string(from: Date())
+    let jsonPathURL = FileManager.documentsDirectoryURL.appendingPathComponent("\(currentDate).json")
+
+    do {
+      if FileManager.default.fileExists(atPath: jsonPathURL.path) {
+        let loadedPhoto = try photoAPIService.loadPhoto(from: jsonPathURL, for: currentDate)
+        photo = loadedPhoto
+      } else {
+        let fetchedPhoto = try await photoAPIService.fetchPhoto(from: currentDate)
+        let savedPhoto = try await photoAPIService.savePhoto(
+          fetchedPhoto,
+          for: currentDate,
+          to: FileManager.documentsDirectoryURL
+        )
+
+        let encoder = JSONEncoder()
+        encoder.outputFormatting = .prettyPrinted
+        let jsonData = try encoder.encode(savedPhoto)
+        try jsonData.write(to: jsonPathURL)
+
+        photo = savedPhoto
+      }
+    } catch {
+      self.error = error
+    }
+
+    isLoading = false
+  }
+}
