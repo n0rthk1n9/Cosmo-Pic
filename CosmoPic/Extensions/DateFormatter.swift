4d5baa414aa9e73e5928be05d2608831ae620213
Jan Armbrust <jan.armbrust@googlemail.com>
2024-02-10 17:26:23 +0100

    move helper function to extension

--- CosmoPic/Extensions/DateFormatter.swift
+++ CosmoPic/Extensions/DateFormatter.swift
@@ -13,4 +13,21 @@ public extension DateFormatter {
     formatter.dateFormat = "yyyy-MM-dd"
     return formatter
   }()
+
+  static func localizedDateString(from dateString: String) -> String {
+    let inputFormatter = DateFormatter()
+    inputFormatter.dateFormat = "yyyy-MM-dd"
+    inputFormatter.locale = Locale(identifier: "en_US_POSIX")
+
+    let outputFormatter = DateFormatter()
+    outputFormatter.dateStyle = .medium
+    outputFormatter.timeStyle = .none
+    outputFormatter.locale = Locale.current
+
+    if let date = inputFormatter.date(from: dateString) {
+      return outputFormatter.string(from: date)
+    } else {
+      return "Invalid Date"
+    }
+  }
 }
