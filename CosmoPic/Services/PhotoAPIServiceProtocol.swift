66667b848b0b9daa4047368a9044a60111fb591a
Jan Armbrust <jan.armbrust@googlemail.com>
2024-02-20 18:31:20 +0100

    adjust function to accept retry handler

--- CosmoPic/Services/PhotoAPIServiceProtocol.swift
+++ CosmoPic/Services/PhotoAPIServiceProtocol.swift
@@ -8,7 +8,7 @@
 import Foundation
 
 protocol PhotoAPIServiceProtocol {
-  func fetchPhoto(from date: String) async throws -> Photo
+  func fetchPhoto(from date: String, retryHandler: (() -> Void)?) async throws -> Photo
   func savePhoto(_ photo: Photo, for date: String, to directory: URL) async throws -> Photo
   func loadPhoto(from jsonPath: URL, for _: String) throws -> Photo
 }
