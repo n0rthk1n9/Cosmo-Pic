06174a7a155ae31dca8ef1b1ec3f5b4a43f52fab
Jan Armbrust <jan.armbrust@googlemail.com>
2024-02-24 16:36:12 +0100

    add new parameters to be able to render hero image in specific cases

--- CosmoPic/Views/GeneralViews/DynamicPhotoView.swift
+++ CosmoPic/Views/GeneralViews/DynamicPhotoView.swift
@@ -9,13 +9,15 @@ import SwiftUI
 
 struct DynamicPhotoView: View {
   let photo: Photo
+  var showAsHeroImage: Bool? = false
+  var size: CGSize? = .init(width: 100, height: 100)
 
   var body: some View {
     if let localFilename = photo.localFilename {
       let localFileURL = FileManager.localFileURL(for: localFilename)
-      PhotoView(url: localFileURL)
+      PhotoView(url: localFileURL, showAsHeroImage: showAsHeroImage, size: size)
     } else if let hdUrl = photo.hdURL {
-      PhotoView(url: hdUrl)
+      PhotoView(url: hdUrl, showAsHeroImage: showAsHeroImage, size: size)
     }
   }
 }
--- CosmoPic/Views/GeneralViews/PhotoView.swift
+++ CosmoPic/Views/GeneralViews/PhotoView.swift
@@ -9,22 +9,42 @@ import SwiftUI
 
 struct PhotoView: View {
   let url: URL
+  var showAsHeroImage: Bool? = false
+  var size: CGSize? = .init(width: 100, height: 100)
 
   var body: some View {
-    AsyncImage(url: url) { image in
-      image
-        .resizable()
-        .aspectRatio(contentMode: .fit)
-    } placeholder: {
-      HStack {
-        Spacer()
-        ProgressView()
-          .frame(height: 300)
-        Spacer()
+    if let showAsHeroImage, let size {
+      if showAsHeroImage {
+        AsyncImage(
+          url: url,
+          content: { image in
+            image
+              .resizable()
+          },
+          placeholder: {
+            ProgressView()
+          }
+        )
+        .scaledToFill()
+        .ignoresSafeArea()
+        .frame(maxWidth: size.width, maxHeight: size.height * 0.60)
+      } else {
+        AsyncImage(url: url) { image in
+          image
+            .resizable()
+            .aspectRatio(contentMode: .fit)
+        } placeholder: {
+          HStack {
+            Spacer()
+            ProgressView()
+              .frame(height: 300)
+            Spacer()
+          }
+        }
+        .cornerRadius(20)
+        .clipped()
+        .padding(.horizontal)
       }
     }
-    .cornerRadius(20)
-    .clipped()
-    .padding(.horizontal)
   }
 }
