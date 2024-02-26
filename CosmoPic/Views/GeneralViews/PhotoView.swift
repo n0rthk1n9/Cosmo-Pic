041f586617860ccc36496625ce2f8996de82d6f0
Jan Armbrust <jan.armbrust@googlemail.com>
2024-02-26 19:26:40 +0100

    fix force unwrap warning

--- CosmoPic/Views/GeneralViews/PhotoView.swift
+++ CosmoPic/Views/GeneralViews/PhotoView.swift
@@ -50,5 +50,13 @@ struct PhotoView: View {
 }
 
 #Preview {
-  PhotoView(url: URL(string: "https://upload.wikimedia.org/wikipedia/commons/thumb/f/fa/Apple_logo_black.svg/1024px-Apple_logo_black.svg.png")!, showAsHeroImage: true)
+  guard let url =
+    URL(
+      string: "https://upload.wikimedia.org/wikipedia/commons/thumb/f/fa/Apple_logo_black.svg/1024px-Apple_logo_black.svg.png"
+    )
+  else {
+    return Text("Error creating url")
+  }
+
+  return PhotoView(url: url, showAsHeroImage: true)
 }
