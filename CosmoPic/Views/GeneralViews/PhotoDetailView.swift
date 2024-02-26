54ef81eac5622edf8e43bbbed0fefa101eaf7718
Jan Armbrust <jan.armbrust@googlemail.com>
2024-02-26 18:04:47 +0100

    remove uneccesary error check

--- CosmoPic/Views/GeneralViews/PhotoDetailView.swift
+++ CosmoPic/Views/GeneralViews/PhotoDetailView.swift
@@ -19,10 +19,8 @@ struct PhotoDetailView: View {
       } else if let photo = viewModel.photo {
         PhotoDetailContentView(photo: photo, fullDetail: true)
       } else {
-        if let error = viewModel.error {
-          ContentUnavailableView("No Data available", systemImage: "x.circle")
-            .showCustomAlert(alert: $viewModel.error)
-        }
+        ContentUnavailableView("No Data available", systemImage: "x.circle")
+          .showCustomAlert(alert: $viewModel.error)
       }
     }
     #if os(visionOS)
