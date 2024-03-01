c15b41b5eac78e8ddad1e810ef93413c250473b3
Jan Armbrust <jan.armbrust@googlemail.com>
2024-03-01 18:36:17 +0100

    remove code duplication

--- CosmoPic/Views/FavoritesView.swift
+++ CosmoPic/Views/FavoritesView.swift
@@ -19,7 +19,6 @@ struct FavoritesView: View {
             .foregroundColor(.gray)
             .padding()
             .multilineTextAlignment(.center)
-            .navigationTitle("Favorites 🌟")
         } else {
           List {
             ForEach(viewModel.favorites, id: \.title) { photo in
@@ -29,13 +28,13 @@ struct FavoritesView: View {
             }
             .onDelete(perform: delete)
           }
-          .navigationTitle("Favorites 🌟")
           .accessibilityIdentifier("favorites-list")
         }
       }
       #if os(visionOS)
       .padding()
       #endif
+      .navigationTitle("Favorites 🌟")
     }
     .onAppear {
       viewModel.loadFavorites()
