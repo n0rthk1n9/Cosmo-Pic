baec063412892436b5d79e5191a69f64cbd84f95
Jan Armbrust <jan.armbrust@googlemail.com>
2024-03-01 19:44:00 +0100

    add new favorites button to history detail view

--- CosmoPic/Views/GeneralViews/PhotoView.swift
+++ CosmoPic/Views/GeneralViews/PhotoView.swift
@@ -18,54 +18,58 @@ struct PhotoView: View {
   var body: some View {
     if let showAsHeroImage, let size {
       if showAsHeroImage {
-        ZStack(alignment: .bottomTrailing) {
-          AsyncImage(
-            url: url,
-            content: { image in
-              image
-                .resizable()
-            },
-            placeholder: {
-              ProgressView()
-            }
-          )
-          .scaledToFill()
-          .ignoresSafeArea()
-          .frame(maxWidth: size.width, maxHeight: size.height * 0.60)
+        ZStack(alignment: .topTrailing) {
+          ZStack(alignment: .bottomTrailing) {
+            AsyncImage(
+              url: url,
+              content: { image in
+                image
+                  .resizable()
+              },
+              placeholder: {
+                ProgressView()
+              }
+            )
+            .scaledToFill()
+            .ignoresSafeArea()
+            .frame(maxWidth: size.width, maxHeight: size.height * 0.60)
 
-          Button(
-            action: {
-              Task {
-                await viewModel.saveImage(from: url)
-              }
-            }, label: {
-              VStack {
-                if viewModel.saveCompleted {
-                  Image(systemName: "checkmark")
-                    .font(.title2)
-                    .foregroundColor(.green)
-                } else {
-                  Label(
-                    title: {
-                      Text("Save")
-                    },
-                    icon: {
-                      Image(systemName: "square.and.arrow.down")
-                    }
-                  )
-                  .font(.title2)
-                  .foregroundColor(viewModel.isSaving ? .gray : .white)
+            Button(
+              action: {
+                Task {
+                  await viewModel.saveImage(from: url)
                 }
+              }, label: {
+                VStack {
+                  if viewModel.saveCompleted {
+                    Image(systemName: "checkmark")
+                      .font(.title2)
+                      .foregroundColor(.green)
+                  } else {
+                    Label(
+                      title: {
+                        Text("Save")
+                      },
+                      icon: {
+                        Image(systemName: "square.and.arrow.down")
+                      }
+                    )
+                    .font(.title2)
+                    .foregroundColor(viewModel.isSaving ? .gray : .white)
+                  }
+                }
+                .frame(width: UIScreen.main.bounds.width / 4)
+                .padding()
+                .background(.thinMaterial)
+                .cornerRadius(10)
+                .padding(EdgeInsets(top: 0, leading: 0, bottom: 20, trailing: 20))
               }
-              .frame(width: UIScreen.main.bounds.width / 4)
-              .padding()
-              .background(.thinMaterial)
-              .cornerRadius(10)
-              .padding(EdgeInsets(top: 0, leading: 0, bottom: 20, trailing: 20))
-            }
-          )
-          .buttonStyle(PlainButtonStyle())
-          .disabled(viewModel.isSaving || viewModel.saveCompleted)
+            )
+            .buttonStyle(PlainButtonStyle())
+            .disabled(viewModel.isSaving || viewModel.saveCompleted)
+          }
+
+          NewFavoriteButtonView(photo: photo)
         }
         .showCustomAlert(alert: $viewModel.error)
       } else {
