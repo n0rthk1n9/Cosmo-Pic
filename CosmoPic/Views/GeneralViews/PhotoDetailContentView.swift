7a951ac6139e68a1e1c3cb4e6f1e2ef83ec42c28
Jan Armbrust <jan.armbrust@googlemail.com>
2024-03-03 11:04:47 +0100

    remove unecessary environment object, remove unecessary parameter

--- CosmoPic/Views/GeneralViews/PhotoDetailContentView.swift
+++ CosmoPic/Views/GeneralViews/PhotoDetailContentView.swift
@@ -8,47 +8,38 @@
 import SwiftUI
 
 struct PhotoDetailContentView: View {
-  @EnvironmentObject var favoritesViewModel: FavoritesViewModel
-
   let photo: Photo
-  var fullDetail = false
 
   var body: some View {
     GeometryReader { geometry in
-      if fullDetail {
-        VStack {
-          ZStack {
-            DynamicPhotoView(photo: photo, showAsHeroImage: true, size: geometry.size)
-          }
-          Form {
-            Section {
-              Text(photo.title)
-                .font(.title)
-                .multilineTextAlignment(.center)
-                .frame(maxWidth: .infinity, alignment: .center)
-            }
-
-            Section {
-              Text(
-                photo.explanation
-                  .replacingOccurrences(of: "  ", with: " ")
-              )
-              .font(.caption)
-              .accessibilityIdentifier("photo-detail-view-photo-explanation")
-            }
-          }
-          .padding(.top, -10)
+      VStack {
+        ZStack {
+          DynamicPhotoView(photo: photo, showAsHeroImage: true, size: geometry.size)
         }
-      }
-    }
+        Form {
+          Section {
+            Text(photo.title)
+              .font(.title)
+              .multilineTextAlignment(.center)
+              .frame(maxWidth: .infinity, alignment: .center)
+          }
 
-    .onAppear {
-      favoritesViewModel.loadFavorites()
+          Section {
+            Text(
+              photo.explanation
+                .replacingOccurrences(of: "  ", with: " ")
+            )
+            .font(.caption)
+            .accessibilityIdentifier("photo-detail-view-photo-explanation")
+          }
+        }
+        .padding(.top, -10)
+      }
     }
   }
 }
 
 #Preview {
-  PhotoDetailContentView(photo: .allProperties, fullDetail: false)
+  PhotoDetailContentView(photo: .allProperties)
     .environmentObject(FavoritesViewModel())
 }
