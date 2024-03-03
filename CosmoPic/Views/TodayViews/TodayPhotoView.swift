a26b37a066a75fcfc8334a9cf37faaed1db7123f
Jan Armbrust <jan.armbrust@googlemail.com>
2024-03-03 11:24:57 +0100

    handle loading of photo directly in today photo view

--- CosmoPic/Views/TodayViews/TodayPhotoView.swift
+++ CosmoPic/Views/TodayViews/TodayPhotoView.swift
@@ -10,9 +10,40 @@ import SwiftUI
 struct TodayPhotoView: View {
   let photo: Photo
 
+  var photoURL: URL? {
+    if let localFilename = photo.localFilename {
+      return FileManager.localFileURL(for: localFilename)
+    } else if let hdUrl = photo.hdURL {
+      return hdUrl
+    } else {
+      return nil
+    }
+  }
+
   var body: some View {
     VStack(alignment: .center) {
-      DynamicPhotoView(photo: photo)
+      GeometryReader { geometry in
+        ZStack(alignment: .topTrailing) {
+          AsyncImage(url: photoURL) { image in
+            image
+              .resizable()
+              .aspectRatio(contentMode: .fill)
+          } placeholder: {
+            HStack {
+              Spacer()
+              ProgressView()
+                .frame(height: 300)
+              Spacer()
+            }
+          }
+          .frame(width: geometry.size.width, height: geometry.size.height)
+          .cornerRadius(20)
+          .clipped()
+
+          FavoriteButtonView(photo: photo)
+        }
+      }
+      .padding(.horizontal, 20)
 
       Text(photo.title)
         .padding([.top, .trailing, .leading])
