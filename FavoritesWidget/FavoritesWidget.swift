a386ce2d30e016ac89f1889a272be2849a3e51d3
Jan Armbrust <jan.armbrust@googlemail.com>
2024-03-22 17:38:30 +0100

    add widget randomly displaying a favorites image

--- FavoritesWidget/FavoritesWidget.swift
+++ FavoritesWidget/FavoritesWidget.swift
@@ -9,23 +9,40 @@ import SwiftUI
 import WidgetKit
 
 struct Provider: TimelineProvider {
+  let viewModel: FavoritesViewModel
+
+  var randomPhoto: UIImage? {
+    guard !viewModel.favorites.isEmpty else { return nil }
+    let randomIndex = Int.random(in: 0 ..< viewModel.favorites.count)
+    let photo = viewModel.favorites[randomIndex]
+
+    if let localFilenameThumbnail = photo.localFilenameThumbnail {
+      let url = FileManager.localFileURL(for: localFilenameThumbnail)
+      if let imageData = try? Data(contentsOf: url) {
+        return UIImage(data: imageData)
+      }
+    } else if let hdUrl = photo.hdURL, let imageData = try? Data(contentsOf: hdUrl) {
+      return UIImage(data: imageData)
+    }
+    return nil
+  }
+
   func placeholder(in _: Context) -> SimpleEntry {
-    SimpleEntry(date: Date(), emoji: "😀")
+    SimpleEntry(date: Date(), photo: randomPhoto)
   }
 
   func getSnapshot(in _: Context, completion: @escaping (SimpleEntry) -> Void) {
-    let entry = SimpleEntry(date: Date(), emoji: "😀")
+    let entry = SimpleEntry(date: Date(), photo: randomPhoto)
     completion(entry)
   }
 
   func getTimeline(in _: Context, completion: @escaping (Timeline<Entry>) -> Void) {
     var entries: [SimpleEntry] = []
 
-    // Generate a timeline consisting of five entries an hour apart, starting from the current date.
     let currentDate = Date()
     for hourOffset in 0 ..< 5 {
       let entryDate = Calendar.current.date(byAdding: .hour, value: hourOffset, to: currentDate)!
-      let entry = SimpleEntry(date: entryDate, emoji: "😀")
+      let entry = SimpleEntry(date: entryDate, photo: randomPhoto)
       entries.append(entry)
     }
 
@@ -36,28 +53,28 @@ struct Provider: TimelineProvider {
 
 struct SimpleEntry: TimelineEntry {
   let date: Date
-  let emoji: String
+  let photo: UIImage?
 }
 
 struct FavoritesWidgetEntryView: View {
   var entry: Provider.Entry
 
   var body: some View {
-    VStack {
-      Text("Time:")
-      Text(entry.date, style: .time)
-
-      Text("Emoji:")
-      Text(entry.emoji)
+    if let photo = entry.photo {
+      Image(uiImage: photo)
+        .resizable()
+        .scaledToFit()
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
     }
   }
 }
 
 struct FavoritesWidget: Widget {
   let kind: String = "FavoritesWidget"
+  let viewModel = FavoritesViewModel()
 
   var body: some WidgetConfiguration {
-    StaticConfiguration(kind: kind, provider: Provider()) { entry in
+    StaticConfiguration(kind: kind, provider: Provider(viewModel: viewModel)) { entry in
       if #available(iOS 17.0, *) {
         FavoritesWidgetEntryView(entry: entry)
           .containerBackground(.fill.tertiary, for: .widget)
@@ -72,9 +89,9 @@ struct FavoritesWidget: Widget {
   }
 }
 
-#Preview(as: .systemSmall) {
-  FavoritesWidget()
-} timeline: {
-  SimpleEntry(date: .now, emoji: "😀")
-  SimpleEntry(date: .now, emoji: "🤩")
-}
+// #Preview(as: .systemSmall) {
+//  FavoritesWidget()
+// } timeline: {
+//  SimpleEntry(date: .now, emoji: "😀")
+//  SimpleEntry(date: .now, emoji: "🤩")
+// }
