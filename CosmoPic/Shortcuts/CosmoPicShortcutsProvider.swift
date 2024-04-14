1199d9209530fcab97c0e6a1b6c299fe021168a9
Jan Armbrust <jan.armbrust@googlemail.com>
2024-04-14 13:59:00 +0200

    100 Days of Cosmo Pic - Day 40 (#34)
    * add new intent to fetch todays photo
    
    * add shortcuts provider to be able to access intent from siri and spotlight

---
+++ CosmoPic/Shortcuts/CosmoPicShortcutsProvider.swift
@@ -0,0 +1,23 @@
+//
+//  CosmoPicShortcutsProvider.swift
+//  CosmoPic
+//
+//  Created by Jan Armbrust on 13.04.24.
+//
+
+import AppIntents
+
+struct CosmoPicShortcutsProvider: AppShortcutsProvider {
+  static var appShortcuts: [AppShortcut] {
+    AppShortcut(
+      intent: FetchPhotoForTodayIntent(),
+      phrases: [
+        "Show today's \(.applicationName) photo",
+        "Show today's photo in \(.applicationName)",
+        "Show today's space photo"
+      ],
+      shortTitle: "Show today's photo",
+      systemImageName: "photo.fill"
+    )
+  }
+}
