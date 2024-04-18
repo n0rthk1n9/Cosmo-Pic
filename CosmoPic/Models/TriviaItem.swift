18649db38e38bfa8718788709c2703b38c1eb211
Jan Armbrust <jan.armbrust@googlemail.com>
2024-04-18 10:58:26 +0200

    add trivia item model

---
+++ CosmoPic/Models/TriviaItem.swift
@@ -0,0 +1,32 @@
+//
+//  TriviaItem.swift
+//  CosmoPic
+//
+//  Created by Jan Armbrust on 18.04.24.
+//
+
+import Foundation
+
+struct TriviaItem: Codable, Equatable, Hashable {
+  let englishName: String?
+
+  enum CodingKeys: String, CodingKey {
+    case englishName
+  }
+}
+
+extension TriviaItem {
+  static func fixture(
+    englishName: String? = "Earth"
+  ) -> TriviaItem {
+    TriviaItem(
+      englishName: englishName
+    )
+  }
+
+  static var allProperties: TriviaItem {
+    .fixture(
+      englishName: "Earth"
+    )
+  }
+}
