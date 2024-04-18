339e600aa99b386295cd7792608ddf0ed3cadd8d
Jan Armbrust <jan.armbrust@googlemail.com>
2024-04-18 10:54:36 +0200

    add trivia view and add it to tabs

---
+++ CosmoPic/Views/TiviaViews/TriviaView.swift
@@ -0,0 +1,18 @@
+//
+//  TriviaView.swift
+//  CosmoPic
+//
+//  Created by Jan Armbrust on 18.04.24.
+//
+
+import SwiftUI
+
+struct TriviaView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TriviaView()
+}
