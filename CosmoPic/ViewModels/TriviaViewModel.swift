7772dd94b0e204689fc87665f6909433f652d8d8
Jan Armbrust <jan.armbrust@googlemail.com>
2024-04-18 11:17:07 +0200

    add view model for trivia view

---
+++ CosmoPic/ViewModels/TriviaViewModel.swift
@@ -0,0 +1,49 @@
+//
+//  TriviaViewModel.swift
+//  CosmoPic
+//
+//  Created by Jan Armbrust on 18.04.24.
+//
+
+import Foundation
+
+@Observable
+class TriviaViewModel {
+  var planets: [TriviaItem] = []
+  var isLoading = false
+
+  var error: CosmoPicError?
+
+  @ObservationIgnored
+  private let soloarSystemAPIService: SoloarSystemAPIServiceProtocol
+
+  init(soloarSystemAPIService: SoloarSystemAPIServiceProtocol = SoloarSystemAPIService()) {
+    self.soloarSystemAPIService = soloarSystemAPIService
+  }
+
+  @MainActor
+  func getPlanets() async {
+    isLoading = true
+    
+    let planetsFilePath = FileManager.appGroupContainerURL.appendingPathComponent("planets.json")
+
+    do {
+      if FileManager.default.fileExists(atPath: planetsFilePath.path) {
+//        planets = try soloarSystemAPIService.loadPlanets()
+        print("loaded from file")
+      } else {
+        let fetchedPlanets = try await soloarSystemAPIService.fetchPlanets()
+
+        // try soloarSystemAPIService.savePlanets()
+        planets = fetchedPlanets
+        print("loaded from web")
+      }
+    } catch let error as CosmoPicError {
+      self.error = error
+    } catch {
+      self.error = .other(error: error)
+    }
+    isLoading = false
+  }
+}
+
