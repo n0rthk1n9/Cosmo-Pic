f4b4f30555a1a016005479ba2f09b757de7afa00
Jan Armbrust <jan.armbrust@googlemail.com>
2024-04-23 17:50:34 +0200

    remove querying only certain data

---
+++ CosmoPic/Models/Mass.swift
@@ -0,0 +1,8 @@
+//
+//  Mass.swift
+//  CosmoPic
+//
+//  Created by Jan Armbrust on 23.04.24.
+//
+
+import Foundation
---
+++ CosmoPic/Models/Vol.swift
@@ -0,0 +1,8 @@
+//
+//  Vol.swift
+//  CosmoPic
+//
+//  Created by Jan Armbrust on 23.04.24.
+//
+
+import Foundation
