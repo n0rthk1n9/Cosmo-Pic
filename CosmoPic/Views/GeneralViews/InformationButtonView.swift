aa30086fa343e326867b26e0c1d5e087f0045aad
Jan Armbrust <jan.armbrust@googlemail.com>
2024-05-12 15:27:12 +0200

    add new info button

---
+++ CosmoPic/Views/GeneralViews/InformationButtonView.swift
@@ -0,0 +1,36 @@
+//
+//  InformationButtonView.swift
+//  CosmoPic
+//
+//  Created by Jan Armbrust on 12.05.24.
+//
+
+import SwiftUI
+
+struct InformationButtonView: View {
+  let photo: Photo
+  var onInfoButtonTapped: () -> Void
+
+  var body: some View {
+    Button(action: {
+      let generator = UIImpactFeedbackGenerator(style: .medium)
+      generator.impactOccurred()
+      onInfoButtonTapped()
+    }, label: {
+      Image(systemName: "info.circle")
+        .font(.title)
+        .foregroundStyle(.white)
+        .frame(width: 32, height: 32)
+        .padding()
+    })
+    .background(.ultraThinMaterial)
+    .clipShape(Circle())
+    .padding()
+  }
+}
+
+#Preview {
+  InformationButtonView(photo: .allProperties) {
+    print("tapped")
+  }
+}
